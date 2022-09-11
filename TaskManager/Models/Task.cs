acd08b8700983f4c9156a06a81225a88ef68bfc3
lipfiggy <csgo.sotar@mail.ru>
2022-09-11 12:30:54 +0300

    add Task Model

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public Guid id { get; set; }
+        [Required]
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+        public DateTime Deadline { get; set; }
+
+    }
+}
