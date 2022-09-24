2fa8105226d1d1f13331c607cc439cfda506a713
JuliaNed26 <nedavnaia@gmail.com>
2022-09-24 22:22:16 +0300

    MVC and web api caching

---
+++ TaskManagerMVC/Controllers/UserLoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.Common;
+using System.Net.Http.Headers;
+using TaskManagerModels;
+
+namespace TaskManagerMVC.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        IConfiguration _config;
+
+        public UserLoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginUser([Bind("Email,Password")] UserLoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using(HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_config["WebApiLink"]);
+                    var responseTask = await client.PostAsJsonAsync<UserLoginModel>("login", loginModel);
+
+                    if (responseTask.IsSuccessStatusCode)
+                    {
+                        var token = responseTask.Content.ReadAsStringAsync().Result;
+                        //remember token
+                        //call groups get method
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Error occured");
+                    }
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
---
+++ TaskManagerMVC/Controllers/WebApiAuthenticationHttpClientHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+using System.Threading;
+
+namespace TaskManagerMVC.Controllers
+{
+    public class WebApiAuthenticationHttpClientHandler:HttpClientHandler
+    {
+        IHttpContextAccessor _accessor;
+        public WebApiAuthenticationHttpClientHandler(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _accessor.HttpContext.GetTokenAsync("access_token");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
