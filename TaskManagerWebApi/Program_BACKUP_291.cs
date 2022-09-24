59d81bd84698e2bf35f1d8316a70553894be2549
lipfiggy <csgo.sotar@mail.ru>
2022-09-24 22:54:44 +0300

    hz

---
+++ TaskManagerWebApi/Program_BACKUP_291.cs
@@ -0,0 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TaskManagerContext>(option => option.UseSqlServer(
+    builder.Configuration.GetConnectionString("TaskManagerConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            //ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+<<<<<<< HEAD
+=======
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+>>>>>>> e7e0098 (Add Register logic, add authorize logic to GroupsController.)
+builder.Services.AddAuthorization();
+
+// Add services to the container.
+builder.Services.AddControllers(); 
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddLazyCache();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllers();
+
+app.UseResponseCaching();
+
+app.Run();
